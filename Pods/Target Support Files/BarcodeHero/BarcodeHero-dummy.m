0fef0da03dd558a965d70bcab40a702776a0ea06
Brian Drelling <brian.drelling@gmail.com>
2019-05-29 21:57:48 -0500

    Checking in Pods

---
+++ Pods/Target Support Files/BarcodeHero/BarcodeHero-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BarcodeHero : NSObject
+@end
+@implementation PodsDummy_BarcodeHero
+@end
---
+++ Pods/Target Support Files/Pods-BarcodeHero-BarcodeHeroTests/Pods-BarcodeHero-BarcodeHeroTests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_BarcodeHero_BarcodeHeroTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_BarcodeHero_BarcodeHeroTestsVersionString[];
+
