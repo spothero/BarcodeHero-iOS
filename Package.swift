d5d587bcd827abe5550cc484239bdd421672c304
Brian Drelling <brian.drelling@gmail.com>
2019-10-01 17:55:17 -0500

    Addressed Package.swift with BarcodeHeroUI

--- Package.swift
+++ Package.swift
@@ -2,6 +2,10 @@
 
 import PackageDescription
 
+// NOTE: BarcodeHeroUI is not yet supported! We're lacking the ability to include Resources in SPM projects.
+//       When this is possible, we need to address all BLOCKED BY SR-2866 sections below
+//       Reference: https://bugs.swift.org/browse/SR-2866
+
 let package = Package(
     name: "BarcodeHero",
     platforms: [
@@ -10,8 +14,11 @@ let package = Package(
     ],
     products: [
         .library(name: "BarcodeHeroCore", targets: ["BarcodeHeroCore"]),
-        .library(name: "BarcodeHeroUI", targets: ["BarcodeHeroUI"]),
-        .library(name: "BarcodeHero", targets: ["BarcodeHeroCore", "BarcodeHeroUI"]),
+        // BLOCKED BY SR-2866 -- Uncomment
+        // .library(name: "BarcodeHeroUI", targets: ["BarcodeHeroUI"]),
+        .library(name: "BarcodeHero", targets: ["BarcodeHeroCore"]),
+        // BLOCKED BY SR-2866 -- Uncomment and delete the previous definition of the BarcodeHero library product
+        // .library(name: "BarcodeHero", targets: ["BarcodeHeroCore", "BarcodeHeroUI"]),
     ],
     dependencies: [],
     targets: [
@@ -19,15 +26,17 @@ let package = Package(
             name: "BarcodeHeroCore",
             dependencies: []
         ),
-        .target(
-            name: "BarcodeHeroUI",
-            dependencies: []
-        ),
+        // BLOCKED BY SR-2866 -- Uncomment
+        // .target(
+        //     name: "BarcodeHeroUI",
+        //     dependencies: []
+        // ),
         .testTarget(
             name: "BarcodeHeroTests",
             dependencies: [
                 .target(name: "BarcodeHeroCore"),
-                .target(name: "BarcodeHeroUI"),
+                // BLOCKED BY SR-2866 -- Uncomment
+                // .target(name: "BarcodeHeroUI"),
             ]
         ),
     ]
